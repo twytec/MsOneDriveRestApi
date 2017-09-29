fa182451ae5fbb746860bebde900ecbd3e5ce23f
Thomas Wycichowski <wycichowski@live.de>
2017-09-30 00:16:30 +0200

    Projektdateien hinzufügen.

---
+++ MsOneDriveRestApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsOneDriveRestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
